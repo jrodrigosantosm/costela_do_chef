2e4cb170463b7e85d82b8583f00aecb3ae243387
Rodrigo Santos <rodrigom376@gmail.com>
2024-09-22 01:02:03 -0300

    link do botão

--- src/app/components/cardapio-digital/cardapio-digital.component.ts
+++ src/app/components/cardapio-digital/cardapio-digital.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./cardapio-digital.component.scss']
 })
 export class CardapioDigitalComponent {
-
+  voltar() {
+    window.location.href = '/link'
+  }
 }
